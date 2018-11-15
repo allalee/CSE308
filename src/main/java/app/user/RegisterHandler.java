dce38d770314ee452428af4f71c9bbd67ff9b89b
Andrew Yu <yandrew72@yahoo.com>
2018-11-14 22:16:49 -0500

    barebones registration

--- src/main/java/app/user/RegisterHandler.java
+++ src/main/java/app/user/RegisterHandler.java
@@ -1,11 +1,13 @@
 package app.user;
 
+import gerrymandering.HibernateManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.Serializable;
 
@@ -18,16 +20,16 @@ import java.io.Serializable;
 public class RegisterHandler {
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public void registration(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email) throws Throwable {
+    public @ResponseBody
+    void registration(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email, HttpServletResponse resp) throws Throwable {
 
+        HibernateManager hm = HibernateManager.getInstance();
         UsersModel usersModel = new UsersModel(username, password, email);
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("lions-jpa");
-        EntityManager em = emf.createEntityManager();
+        boolean persisted = hm.persistToDB(usersModel);
+        System.out.println(persisted);
 
-        em.getTransaction().begin();
-        em.persist(usersModel);
-        em.getTransaction().commit();
 
+        resp.sendRedirect("./login.html");
     }
 
 }
