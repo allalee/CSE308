d554e64b424b5f7a936f9fb6e4ed9fa3b0650567
changcliu <changchieh.liu@stonybrook.edu>
2018-11-18 16:30:27 -0500

    Added most getters and setters

--- src/main/java/app/ElectionData.java
+++ src/main/java/app/ElectionData.java
@@ -1,10 +1,17 @@
 package app;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class ElectionData {
+    //ElectionData needs to be preprocessed before hand
     private HashMap<Parties, Integer> voterDistribution;
-    public ElectionData (){
+    private int electionYear;
+    private ElectionType electionType;
+    private int totalVotes;
+    private List<Representative> reps;
+    private Parties winner;
+    public ElectionData(){
 
     }
     public int getNumVotesForDem() {
@@ -12,5 +19,39 @@ public class ElectionData {
     }
     public int getNumVotesForRep() {
         return voterDistribution.get(Parties.REPUBLICAN);
+    }
+    public HashMap<Parties, Integer> getVoterDistribution() {
+        return voterDistribution;
+    }
+    public int getElectionYear(){
+        return electionYear;
+    }
+    public ElectionType getElectionType() {
+        return electionType;
+    }
+    public int getTotalVotes() {
+        return totalVotes;
+    }
+    public List<Representative> getReps() {
+        return reps;
+    }
+    public Parties getWinner() {
+        return winner;
+    }
+
+    public void setVoterDistribution(HashMap<Parties, Integer> voterDistribution) {
+        this.voterDistribution = voterDistribution;
+    }
+
+    public void setTotalVotes(int totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+
+    public void setReps(List<Representative> reps) {
+        this.reps = reps;
+    }
+
+    public void setWinner(Parties winner) {
+        this.winner = winner;
     }
 }
