87aaeafb60ea4bd767d3c02e55cf7fba6fcfa99a
Yixiu Liu <yixiu.liu@stonybrook.edu>
2018-11-27 18:21:22 -0600

    befor merge

---
+++ src/main/java/app/json/ERM.java
@@ -0,0 +1,19 @@
+package app.json;
+
+/**
+ * Created by Yixiu Liu on 11/27/2018.
+ */
+public class ERM {
+
+    public static String get(){
+        return "";
+    }
+}
+
+enum C{
+    URL, PARAM
+}
+
+enum V{
+    REQUEST,
+}
