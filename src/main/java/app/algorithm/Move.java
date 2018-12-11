5e9395d2c688d25af9987916732ebf357da909ba
Yixiu Liu <yixiu.liu@stonybrook.edu>
2018-12-10 21:10:23 -0600

    Algorithm can run on the result of manual move

--- src/main/java/app/algorithm/Move.java
+++ src/main/java/app/algorithm/Move.java
@@ -19,9 +19,6 @@ public class Move {
     private District src;
     private District dest;
     private Precinct precinct;
-    
-    private Geometry srcOriginalGeometry;
-    private Geometry destOriginalGeometry;
 
     //Annealing Constructor
     public Move(District src, District dest, Precinct precinct) {
@@ -31,9 +28,6 @@ public class Move {
         this.precinctID = precinct.getID();
         this.srcDistrict = src.getID();
         this.destDistrict = dest.getID();
-
-        srcOriginalGeometry = src.getCurrentGeometry();
-        destOriginalGeometry = dest.getCurrentGeometry();
     }
     //Region Growing Constructor
     public Move(District dest, Precinct precinct){
