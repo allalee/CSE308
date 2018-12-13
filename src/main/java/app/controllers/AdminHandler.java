9ece08720b0b5e7cb0fc7145f8a8e846e4c68b04
Andrew Yu <yandrew72@yahoo.com>
2018-12-13 12:56:04 -0500

    admin delete configured with foreign key constraint deletion

--- src/main/java/app/controllers/AdminHandler.java
+++ src/main/java/app/controllers/AdminHandler.java
@@ -1,5 +1,7 @@
 package app.controllers;
 
+import app.user.Maps;
+import app.user.Preferences;
 import app.user.UsersModel;
 import gerrymandering.HibernateManager;
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Andrew on 11/18/2018.
@@ -97,11 +101,44 @@ public class AdminHandler {
 
         //only if the account exists we remove it.
         if(!username.equals("") && !password.equals("")) {
+            //remove account's associated preferences and maps first.
+            List<Object> prefList = getUserPreferences(email);
+            int index = 0;
+            while(index < prefList.size()) {
+                Preferences thisPreference = (Preferences) prefList.get(index);
+                hm.removeFromDB(thisPreference);
+                index = index + 1;
+            }
+            //remove account's associated maps
+            List<Object> mapsList = getUserMaps(email);
+            index = 0;
+            while(index < mapsList.size()) {
+                Maps thisMap = (Maps) mapsList.get(index);
+                hm.removeFromDB(thisMap);
+                index = index + 1;
+            }
             UsersModel usersModel = new UsersModel(username, password, email, "user");
             boolean persisted = hm.removeFromDB(usersModel);
             System.out.println(persisted);
         }
 
         return "redirect:http://localhost:8080/admin";
+    }
+
+
+    public List<Object> getUserPreferences(String email) throws Throwable {
+        HibernateManager hm = HibernateManager.getInstance();
+        Map<String, Object> criteria = new HashMap<>();
+        criteria.put("email", email);
+        List<Object> prefList = hm.getRecordsBasedOnCriteria(Preferences.class, criteria);
+        return prefList;
+    }
+
+    public List<Object> getUserMaps(String email) throws Throwable{
+        HibernateManager hm = HibernateManager.getInstance();
+        Map<String, Object> criteria = new HashMap<>();
+        criteria.put("email", email);
+        List<Object> mapsList = hm.getRecordsBasedOnCriteria(Maps.class, criteria);
+        return mapsList;
     }
 }
